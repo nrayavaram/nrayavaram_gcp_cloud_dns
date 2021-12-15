132d7eb84cae83dd92f092e5faba3f6b6721acc4
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-12-15 16:25:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,39 +3,36 @@ provider "google" {
  project = "airline1-sabre-wolverine"
 }
 
-
-
-
-resource "google_dns_policy" "example-policy" {
-  name                      = "example-policy"
-  enable_inbound_forwarding = true
-
-  enable_logging = true
-
-  alternative_name_server_config {
-    target_name_servers {
-      ipv4_address    = "172.16.1.10"
-      forwarding_path = "private"
-    }
-    target_name_servers {
-      ipv4_address = "172.16.1.20"
-    }
+resource "google_dns_managed_zone" "private-zone" {
+  name        = "wf-us-prod-dns-gcp-res123"
+  dns_name    = "wf-us-prod-dns-gcp-res123"
+  description = "Example private DNS zone"
+  labels = {
+    foo = "bar"
   }
 
-  networks {
-    network_url = google_compute_network.network-1.id
-  }
-  networks {
-    network_url = google_compute_network.network-2.id
+  visibility = "private"
+
+  private_visibility_config {
+    networks {
+      network_url = google_compute_network.network-1.id
+    }
+    networks {
+      network_url = google_compute_network.network-2.id
+    }
   }
 }
 
 resource "google_compute_network" "network-1" {
-  name                    = "network-1"
+  name                    = "wf-us-prod-dns-gcp-net123"
+  mtu                     = 1500
   auto_create_subnetworks = false
+  delete_default_routes_on_create = true
 }
 
 resource "google_compute_network" "network-2" {
-  name                    = "network-2"
+  name                    = "wf-us-prod-dns-gcp-net223"
+  mtu                     = 1500
   auto_create_subnetworks = false
+  delete_default_routes_on_create = true
 }
